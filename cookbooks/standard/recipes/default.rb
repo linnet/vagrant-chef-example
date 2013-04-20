dfef7aac68ab722bab846b3288cc4816e979a5b9
Jesper Kamstrup Linnet <jesper@kamstrup-linnet.dk>
2013-04-20 21:21:23 +0200

    Updated to use the opscode rsync cookbook instead

--- cookbooks/standard/recipes/default.rb
+++ cookbooks/standard/recipes/default.rb
@@ -1,23 +1,38 @@
-apt_package "screen" do
-	action :install
+# apt_package "screen" do
+# 	action :install
+# end
+
+# cookbook_file "/etc/rsyncd.conf" do
+# 	action :create_if_missing
+# 	mode 0644
+# end
+
+# cookbook_file "/etc/rsyncd.secrets" do
+# 	action :create_if_missing
+# 	mode 0600
+# end
+
+# cookbook_file "/etc/default/rsync" do
+# 	action :create
+# 	mode 0644
+# end
+
+# service "rsync" do
+# 	supports :status => true, :start => true, :stop => true, :restart => true
+# 	action [:enable, :start]
+# end
+
+
+
+
+include_recipe "rsync::server"
+
+#simple server this directory up read only
+rsync_serve "tmp" do 
+  path "/tmp"
+  read_only true
+  uid "nobody"
+  gid "nobody"
 end
 
-cookbook_file "/etc/rsyncd.conf" do
-	action :create_if_missing
-	mode 0644
-end
 
-cookbook_file "/etc/rsyncd.secrets" do
-	action :create_if_missing
-	mode 0600
-end
-
-cookbook_file "/etc/default/rsync" do
-	action :create
-	mode 0644
-end
-
-service "rsync" do
-	supports :status => true, :start => true, :stop => true, :restart => true
-	action [:enable, :start]
-end
